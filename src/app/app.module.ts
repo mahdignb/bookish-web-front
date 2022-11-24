cb7c51cb412ab7f9617b0735962c407a819d4c7a
Mahdi Ghanbari <qanbarim326@gmail.com>
2022-11-24 22:22:21 +0330

    Added routing to the app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { LoginPageComponent } from './routes/login-page/login-page.component';
+
+const routes: Routes = [{ path: 'login', component: LoginPageComponent }];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, LoginPageComponent],
+  imports: [BrowserModule, RouterModule.forRoot(routes)],
+  exports: [RouterModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
